7b7ba0a89f4702d039d5211fedfe3cc8bad45aae
Camila cerón <claudia.ceron@correounivalle.edu.co>
2023-09-27 14:00:00 -0500

    Initial commit

---
+++ Principal.java
@@ -0,0 +1,18 @@
+
+package miniproyecto1;
+import modelo.Ventana;
+
+
+
+public class Principal {
+
+   
+    public static void main(String[] args) {
+      //creo el objeto ventana (JFrame)
+      
+	Ventana ventana = new Ventana();
+	
+       
+    }
+    
+}
